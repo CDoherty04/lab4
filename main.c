975e9304d83ece76a471eeaf7fc30855aa68ea2e
Charlie <charlieedoherty@gmail.com>
2024-10-06 18:09:47 -0500

    Implement basic input and output

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+    int score;
+
+    // Input the desired score
+    printf("Enter the desired NFL score: ");
+    scanf("%d", &score);
+
+    printf("All combinations for a score of %d:\n", score);
+}
