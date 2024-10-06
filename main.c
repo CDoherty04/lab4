5bfb453d8c3691a410a9bcf1d8bb87b99a91cad9
Charlie <charlieedoherty@gmail.com>
2024-10-06 18:11:43 -0500

    Add conversions as ints

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+void find_combinations(int score) {
+    int td_2pc = 8;  // Touchdown + 2-point conversion
+    int td_pat = 7;  // Touchdown + PAT
+    int td = 6;      // Touchdown
+    int fg = 3;      // Field Goal
+    int safety = 2;  // Safety
+
+    // Use nested loops to find each possible combination?
+    
+}
+
 void main() {
     int score;
 
@@ -8,4 +19,5 @@ void main() {
     scanf("%d", &score);
 
     printf("All combinations for a score of %d:\n", score);
+    find_combinations(score);
 }
