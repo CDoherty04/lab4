5c6ed6b0e6affd8ce03f56d27a6d7f71fb64944b
Charlie <charlieedoherty@gmail.com>
2024-10-06 18:12:32 -0500

    Print out every combination

--- main.c
+++ main.c
@@ -7,8 +7,23 @@ void find_combinations(int score) {
     int fg = 3;      // Field Goal
     int safety = 2;  // Safety
 
-    // Use nested loops to find each possible combination?
-    
+    // Use nested loops to find each possible combination
+    for (int n_td_2pc = 0; n_td_2pc * td_2pc <= score; ++n_td_2pc) {
+        for (int n_td_pat = 0; n_td_pat * td_pat <= score; ++n_td_pat) {
+            for (int n_td = 0; n_td * td <= score; ++n_td) {
+                for (int n_fg = 0; n_fg * fg <= score; ++n_fg) {
+                    for (int n_safety = 0; n_safety * safety <= score; ++n_safety) {
+                        int total = n_td_2pc * td_2pc + n_td_pat * td_pat + n_td * td + n_fg * fg + n_safety * safety;
+
+                        if (total == score) {
+                            printf("%d TD + 2pt, %d TD + PAT, %d TD, %d 3pt FG, %d Safety\n",
+                                   n_td_2pc, n_td_pat, n_td, n_fg, n_safety);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 void main() {
